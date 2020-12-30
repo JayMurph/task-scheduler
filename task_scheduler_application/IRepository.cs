4d86d677c2a73ace032bc03c59bddceafe2b0959
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2020-12-30 11:46:23 -0500

    - created IRepository

---
+++ task_scheduler_application/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_application {
+    /// <summary>
+    /// Interface for classes that interact with a database 
+    /// </summary>
+    /// <typeparam name="T">Type that is created, updated, retrieved, and deleted from a
+    /// data-source</typeparam>
+    interface IRepository<T>{
+        bool Insert(IEnumerable<T> t);
+        IEnumerable<T> Select(Predicate<T> predicate);
+        bool Update(IEnumerable<T> t);
+        bool Delete(IEnumerable<T> t);
+    }
+}
