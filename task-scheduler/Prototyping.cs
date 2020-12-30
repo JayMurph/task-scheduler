20a6828e778416709083ad71bdaa4d6c88bd72bc
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2020-12-30 14:05:14 -0500

    - moved ConstantPeriod out of prototyping and into task_scheduler_application

--- task-scheduler/Prototyping.cs
+++ task-scheduler/Prototyping.cs
@@ -18,26 +18,6 @@ namespace task_scheduler {
             get { return DateTime.Now; }
         }
     }
-    public class ConstantPeriod : INotificationPeriod {
-        private readonly TimeSpan period; 
-
-        public ConstantPeriod(TimeSpan period) {
-            this.period = period;
-        }
-        public DateTime NextNotificationTime(DateTime taskStartTime, DateTime now) {
-            TimeSpan periodAccum = period;
-
-            while(taskStartTime + periodAccum <= now) {
-                periodAccum = periodAccum.Add(period);
-            }
-
-            return taskStartTime + periodAccum;
-        }
-
-        public TimeSpan TimeUntilNextNotification(DateTime taskStartTime, DateTime now) {
-            return now.Subtract(NextNotificationTime(taskStartTime, now));
-        }
-    }
 
     class Prototyping {
 
---
+++ task_scheduler_application/ConstantPeriod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using task_scheduler_entities;
+
+namespace task_scheduler_application {
+    public class ConstantPeriod : INotificationPeriod {
+        private readonly TimeSpan period; 
+
+        public ConstantPeriod(TimeSpan period) {
+            this.period = period;
+        }
+        public DateTime NextNotificationTime(DateTime taskStartTime, DateTime now) {
+            TimeSpan periodAccum = period;
+
+            while(taskStartTime + periodAccum <= now) {
+                periodAccum = periodAccum.Add(period);
+            }
+
+            return taskStartTime + periodAccum;
+        }
+
+        public TimeSpan TimeUntilNextNotification(DateTime taskStartTime, DateTime now) {
+            return now.Subtract(NextNotificationTime(taskStartTime, now));
+        }
+    }
+}
