0237863189c88fcd423cc40f99800ba6bad49887
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-06 12:36:19 -0500

    - new IRepository

---
+++ task-scheduler-data-access/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_scheduler_data_access {
+    public interface IRepository<T> {
+        //this would be nice if I knew how to implement filter and where expressions as parameters
+        //IEnumerable<T> Select();
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        bool Update(T t);
+        bool Add(T t);
+        bool Delete(T t);
+        bool Delete(object id);
+    }
+}
