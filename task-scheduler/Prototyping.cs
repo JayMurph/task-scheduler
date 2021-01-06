5dd74f1c7dcd340e285986b0893c22ccdb9d5a0d
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-05 19:27:35 -0500

    -creating SQLite database to build data-access layer around

--- task-scheduler/Prototyping.cs
+++ task-scheduler/Prototyping.cs
@@ -17,10 +17,44 @@ namespace task_scheduler {
     class Prototyping {
 
         static void Main(string[] args) {
-            using (var conn = new SQLiteConnection("Data Source=testdb.db")) {
-                using (var command = new SQLiteCommand("INSERT INTO Tasks VALUES('blah','blah','blah','blah')", conn)) {
+
+            using (var conn = new SQLiteConnection("Data Source=../../testdb.db")) {
+
+                using (var insertCommand = 
+                    new SQLiteCommand($"INSERT INTO Tasks VALUES('{Guid.NewGuid()}', 'TestTitle', 'TestDescription', '{DateTime.Now}')", conn)) {
+
                     conn.Open();
-                    command.ExecuteNonQuery();
+                    insertCommand.ExecuteNonQuery();
+                    conn.Close();
+                }
+
+                using (var queryCommand = new SQLiteCommand("SELECT * FROM Tasks", conn)) {
+                    conn.Open();
+
+                    using (var reader = queryCommand.ExecuteReader()) {
+
+                        while (reader.Read()) {
+
+                            Guid id = Guid.Parse((string)reader["Id"]);
+                            string title = (string)reader["Title"];
+                            string description = (string)reader["Description"];
+                            DateTime startTime = DateTime.Parse((string)reader["StartTime"]);
+
+                            Console.WriteLine($"Id = {id}");
+                            Console.WriteLine($"Title = {title}");
+                            Console.WriteLine($"Description = {description}");
+                            Console.WriteLine($"StartTime = {startTime}");
+                        }
+                    }
+
+                    conn.Close();
+                }
+
+                using (var deleteCommand = new SQLiteCommand("DELETE FROM Tasks", conn)) {
+                    conn.Open();
+
+                    deleteCommand.ExecuteNonQuery();
+
                     conn.Close();
                 }
             }
