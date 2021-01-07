fd58b077b0ade9ba935a3e29e4045943b2544631
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-07 17:48:07 -0500

    - ReviewFrequency

---
+++ task_scheduler_application/Frequencies/ReviewFrequency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_application.Frequencies {
+    class ReviewFrequency : IDescriptiveNotificationFrequency {
+        private readonly TimeSpan startingPeriod = new TimeSpan(12, 0, 0);
+        public string Description { get => "Review"; }
+
+        public DateTime NextNotificationTime(DateTime taskStartTime, DateTime now) {
+            TimeSpan periodAccum = startingPeriod;
+
+            while(taskStartTime + periodAccum <= now) {
+                periodAccum = periodAccum.Add(periodAccum);
+            }
+
+            return taskStartTime + periodAccum;
+        }
+
+        public TimeSpan TimeUntilNextNotification(DateTime taskStartTime, DateTime now) {
+            return FrequencyUtility.TimeUntilNextNotification(taskStartTime, now, NextNotificationTime);
+        }
+    }
+}
