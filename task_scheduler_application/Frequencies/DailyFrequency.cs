ed1d230b2a7338fe5c2126b62c9222da7cb3c815
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-07 17:32:33 -0500

    - DailyFrequency

---
+++ task_scheduler_application/Frequencies/DailyFrequency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_application.Frequencies {
+    class DailyFrequency : IDescriptiveNotificationFrequency {
+        private readonly TimeSpan period = new TimeSpan(1, 0, 0, 0);
+        public string Description { get => "Daily"; }
+
+        public DateTime NextNotificationTime(DateTime taskStartTime, DateTime now) {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan TimeUntilNextNotification(DateTime taskStartTime, DateTime now) {
+            throw new NotImplementedException();
+        }
+    }
+}
