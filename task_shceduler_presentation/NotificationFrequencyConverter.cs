3477181e7467cd88e47304964fbe8d9393cd501a
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-08 15:24:43 -0500

    - NotificationFrequencyConverter - TaskItemControl

---
+++ task_shceduler_presentation/NotificationFrequencyConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace task_scheduler_presentation {
+    public class NotificationFrequencyConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            if(value is TimeSpan) {
+                TimeSpan time = (TimeSpan)value;
+
+                if(time != TimeSpan.Zero) {
+                    return time.ToString("c");
+                }
+                else {
+                    return "";
+                }
+            }
+            else {
+                string timeStr = (string)value;
+                if(timeStr == TimeSpan.Zero.ToString()) {
+                    return "";
+                }
+                else {
+                    return value.ToString();
+                }
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            return TimeSpan.Parse((string)value);
+        }
+    }
+}
