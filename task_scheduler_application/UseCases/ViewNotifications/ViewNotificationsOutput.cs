d677a9d2d02977c87111e8129868234c30c6dc1a
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-10 00:03:07 -0500

    - ViewNotificationsOutput.cs

---
+++ task_scheduler_application/UseCases/ViewNotifications/ViewNotificationsOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using task_scheduler_application.DTO;
+
+namespace task_scheduler_application.UseCases.ViewNotifications {
+    public class ViewNotificationsOutput : UseCaseOutput {
+        public List<NotificationDTO> Notifications { get; set; } = new List<NotificationDTO>();
+    }
+}
