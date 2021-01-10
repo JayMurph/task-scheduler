07457f66365d853d7abdf18c6f348b9472c20e35
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-09 23:56:51 -0500

    - NotificationDTO.cs

---
+++ task_scheduler_application/DTO/NotificationDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_application.DTO {
+    public class NotificationDTO {
+        Guid Id { get; set; }
+        TaskItemDTO TaskItemDTO { get; set; }
+        DateTime Time { get; set; }
+    }
+}
