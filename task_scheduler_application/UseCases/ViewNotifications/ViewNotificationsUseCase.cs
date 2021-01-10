5beb2853a7dd15cf8c9179ebe49370b0e5566dbb
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-10 18:01:51 -0500

    - ViewNotificationUseCase stub

---
+++ task_scheduler_application/UseCases/ViewNotifications/ViewNotificationsUseCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_application.UseCases.ViewNotifications {
+    public class ViewNotificationsUseCase : IUseCase<ViewNotificationsInput, ViewNotificationsOutput> {
+        public ViewNotificationsInput Input { set; private get; }
+
+        public ViewNotificationsOutput Output { get; private set; }
+
+        public void Execute() {
+            //no input for use case 
+
+
+
+        }
+    }
+}
