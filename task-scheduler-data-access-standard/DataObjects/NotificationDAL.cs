88074bd1d791d237511f2f5785f73b3561092e72
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-11 18:45:46 -0500

    - properties in NotificationFrequencyDAL and NotificationDAL

--- task-scheduler-data-access-standard/DataObjects/NotificationDAL.cs
+++ task-scheduler-data-access-standard/DataObjects/NotificationDAL.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace task_scheduler_data_access_standard.DataObjects {
-    class NotificationDAL {
+    public class NotificationDAL {
+        public NotificationDAL(Guid taskId, DateTime time) {
+            TaskId = taskId;
+            Time = time;
+        }
+
+        public Guid TaskId { get; }
+        public DateTime Time { get; }
     }
 }
--- task-scheduler-data-access-standard/DataObjects/NotificationFrequencyDAL.cs
+++ task-scheduler-data-access-standard/DataObjects/NotificationFrequencyDAL.cs
@@ -4,5 +4,12 @@ using System.Text;
 
 namespace task_scheduler_data_access_standard.DataObjects {
     public class NotificationFrequencyDAL {
+        public NotificationFrequencyDAL(Guid taskId, TimeSpan time) {
+            TaskId = taskId;
+            Time = time;
+        }
+
+        public Guid TaskId { get; }
+        public TimeSpan Time { get; }
     }
 }
