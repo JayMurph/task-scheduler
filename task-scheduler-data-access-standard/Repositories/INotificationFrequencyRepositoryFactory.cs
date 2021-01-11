1ca9bb18975b4dea63db158b7ee0e241617b400a
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-11 18:50:40 -0500

    - INotificationFrequencyRepository

---
+++ task-scheduler-data-access-standard/Repositories/INotificationFrequencyRepositoryFactory.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_data_access_standard.Repositories {
+    public interface INotificationFrequencyRepositoryFactory : IRepositoryFactory<INotificationFrequencyRepository> {
+    }
+}
