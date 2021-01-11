17f5101b1fc0bbd663c69772c8ace2048abea326
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-11 18:53:01 -0500

    - NotificationFrequencyRepository.cs created

---
+++ task-scheduler-data-access-standard/Repositories/NotificationFrequencyRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using task_scheduler_data_access_standard.DataObjects;
+
+namespace task_scheduler_data_access_standard.Repositories {
+    class NotificationFrequencyRepository : INotificationFrequencyRepository {
+        public bool Add(NotificationFrequencyDAL notificationFrequency) {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(NotificationFrequencyDAL notificationFrequency {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(object id) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NotificationFrequencyDAL> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public NotificationFrequencyDAL GetById(object id) {
+            throw new NotImplementedException();
+        }
+
+        public void Save() {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(NotificationFrequencyDAL notificationFrequency) {
+            throw new NotImplementedException();
+        }
+    }
+}
