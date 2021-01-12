d9accde20213e862286ffb6349d7bf527616deff
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-11 19:44:51 -0500

    - removing 'bad' interface requirements from IRepository

--- task-scheduler-data-access-standard/Repositories/IRepository.cs
+++ task-scheduler-data-access-standard/Repositories/IRepository.cs
@@ -7,14 +7,10 @@ using System.Threading.Tasks;
 
 namespace task_scheduler_data_access_standard.Repositories {
     public interface IRepository<T> : IDisposable {
-        //this would be nice if I knew how to implement filter and where expressions as parameters
-        //IEnumerable<T> Select();
         IEnumerable<T> GetAll();
-        T GetById(object id);
         bool Update(T t);
         bool Add(T t);
         bool Delete(T t);
-        bool Delete(object id);
         void Save();
     }
 }
