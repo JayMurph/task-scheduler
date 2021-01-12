df75cb6c5b8aba2ba391e29d0e441ec793531dc8
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-11 19:38:46 -0500

    - NotificationFrequencyRepositoryFactory implentation - test project for data-access - NotificationFrequencyRepositoryTests.cs

---
+++ task-scheduler-data-access-standard/Repositories/NotificationFrequencyRepositoryFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_data_access_standard.Repositories {
+    public class NotificationFrequencyRepositoryFactory : INotificationFrequencyRepositoryFactory {
+        private readonly string connectionStr;
+
+        public NotificationFrequencyRepositoryFactory(string connectionStr) {
+            this.connectionStr = connectionStr;
+        }
+
+        public INotificationFrequencyRepository New() {
+            return new NotificationFrequencyRepository(connectionStr);
+        }
+    }
+}
