4b6a1928478247f11247fe564555dd1dab6a436c
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-11 20:44:09 -0500

    - INotificationFrequencyRepositoryFactory is dependency of ITaskItemRepositoryFactory

--- task-scheduler-data-access-standard/Repositories/TaskItemRepositoryFactory.cs
+++ task-scheduler-data-access-standard/Repositories/TaskItemRepositoryFactory.cs
@@ -5,13 +5,16 @@ using System.Text;
 namespace task_scheduler_data_access_standard.Repositories {
     public class TaskItemRepositoryFactory : ITaskItemRepositoryFactory {
         private readonly string connectionStr;
+        private readonly INotificationFrequencyRepositoryFactory notificationFrequencyRepositoryFactory;
 
-        public TaskItemRepositoryFactory(string connectionStr) {
+        public TaskItemRepositoryFactory(string connectionStr, INotificationFrequencyRepositoryFactory notificationFrequencyRepositoryFactory) {
             this.connectionStr = connectionStr;
+            this.notificationFrequencyRepositoryFactory = 
+                notificationFrequencyRepositoryFactory ?? throw new ArgumentNullException(nameof(notificationFrequencyRepositoryFactory));
         }
 
         public ITaskItemRepository New() {
-            return new TaskItemRepository(connectionStr);
+            return new TaskItemRepository(connectionStr, notificationFrequencyRepositoryFactory);
         }
     }
 }
