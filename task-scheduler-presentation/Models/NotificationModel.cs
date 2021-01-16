941b23178efda5aba0dfd60d65545f37a426a3eb
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-15 21:07:44 -0500

    - created NotificationModel

---
+++ task-scheduler-presentation/Models/NotificationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_scheduler_presentation.Models {
+    /// <summary>
+    /// Centralizes the contents of a Notification that will be presented to the user
+    /// </summary>
+    public class NotificationModel {
+        /// <summary>
+        /// The Title of the Task that generated the Notification
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The time at which the Notification was generated
+        /// </summary>
+        public DateTime Time { get; set; }
+    }
+}
