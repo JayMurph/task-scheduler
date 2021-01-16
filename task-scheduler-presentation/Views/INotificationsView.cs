cf21a4b824a65c183f6f8f7c52276fbbecfb93f0
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-15 21:50:59 -0500

    - created INotificationsView

---
+++ task-scheduler-presentation/Views/INotificationsView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task_scheduler_presentation.Models;
+
+namespace task_scheduler_presentation.Views {
+
+    /// <summary>
+    /// Defines the necessities for a page that displays Notifications
+    /// </summary>
+    public interface INotificationsView {
+
+        /// <summary>
+        /// Collection of Notifications that the page will display
+        /// </summary>
+        ObservableCollection<NotificationModel> Notifications { get; set; }
+
+        /// <summary>
+        /// Callback to be invoked whenever a new Notification is to be added to the pages
+        /// collection.
+        /// </summary>
+        /// <param name="source">
+        /// The object that called the event.
+        /// </param>
+        /// <param name="newNotification">
+        /// A newly generated Notification
+        /// </param>
+        void NotificationCreatedCallback(object source, NotificationModel newNotification);
+
+        /// <summary>
+        /// Executed when the NotificationsView page is closing
+        /// </summary>
+        event EventHandler Closing;
+    }
+}
