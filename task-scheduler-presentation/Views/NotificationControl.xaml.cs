47439de9235de520cc1321b37c1f30c5bfdc3803
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-15 21:15:53 -0500

    - created NotificationControl

---
+++ task-scheduler-presentation/Views/NotificationControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+using task_scheduler_presentation.Models;
+
+namespace task_scheduler_presentation.Views {
+    public sealed partial class NotificationControl : UserControl {
+
+        /// <summary>
+        /// Serves as the Data that the Control markup accesses and displays
+        /// </summary>
+        public NotificationModel Notification {
+            get => this.DataContext as NotificationModel;
+        }
+
+        public NotificationControl() {
+            this.InitializeComponent();
+
+            //Update the Bindings whenever the DataContext is changed
+            this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+    }
+}
