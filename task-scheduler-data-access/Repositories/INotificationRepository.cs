8b3bffd7feeb5d1730ddf1753300cf2bf1ed2603
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-16 16:53:38 -0500

    - created INotificationRepository

---
+++ task-scheduler-data-access/Repositories/INotificationRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using task_scheduler_data_access.DataObjects;
+
+namespace task_scheduler_data_access.Repositories {
+    public interface INotificationRepository : IRepository<NotificationDAL> {
+    }
+}
