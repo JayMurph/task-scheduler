43c65b330c120411ac2380e48b65265b751656ee
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-16 18:20:58 -0500

    - constructor

--- task-scheduler-application/UseCases/ViewNotifications/ViewNotificationsUseCaseFactory.cs
+++ task-scheduler-application/UseCases/ViewNotifications/ViewNotificationsUseCaseFactory.cs
@@ -9,9 +9,12 @@ namespace task_scheduler_application.UseCases.ViewNotifications {
         INotificationManager notificationManager;
         ITaskItemRepositoryFactory taskItemRepositoryFactory;
 
-        public ViewNotificationsUseCaseFactory(INotificationManager notificationManager, ITaskItemRepositoryFactory taskItemRepositoryFactory) {
-            this.notificationManager = notificationManager;
-            this.taskItemRepositoryFactory = taskItemRepositoryFactory;
+        public ViewNotificationsUseCaseFactory(
+            INotificationManager notificationManager,
+            ITaskItemRepositoryFactory taskItemRepositoryFactory) {
+
+            this.notificationManager = notificationManager ?? throw new ArgumentNullException(nameof(notificationManager));
+            this.taskItemRepositoryFactory = taskItemRepositoryFactory ?? throw new ArgumentNullException(nameof(taskItemRepositoryFactory));
         }
 
         public ViewNotificationsUseCase New() {
