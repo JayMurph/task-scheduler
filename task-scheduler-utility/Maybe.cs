902bca1f095a21c0767b9566821e0693fd65d250
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-18 21:50:07 -0500

    documentation

--- task-scheduler-utility/Maybe.cs
+++ task-scheduler-utility/Maybe.cs
@@ -1,26 +1,47 @@
 ﻿using System;
 
 namespace task_scheduler_utility {
+    /// <summary>
+    /// Holds a reference or value type that may or may not be initialized.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Maybe<T> {
 
-        private readonly T t;
+        /// <summary>
+        /// Indicates if the Maybe has been initialized with a value
+        /// </summary>
+        public bool HasValue { get; private set; } 
 
-        public bool HasValue { get; private set; }
-        public T Value { get => t; }
+        /// <summary>
+        /// Returns the object that the Maybe holds
+        /// </summary>
+        public T Value { get; private set; }
 
-        private Maybe(T t) {
-            this.t = t;
+        private Maybe(T value) {
+            Value = value;
             HasValue = true;
         }
 
         private Maybe() {
-
+            HasValue = false;
         }
 
-        public static Maybe<T> Create(T t) {
-            return new Maybe<T>(t);
+        /// <summary>
+        /// Creates and initializes the Maybe with a value
+        /// </summary>
+        /// <param name="value">
+        /// </param>
+        /// <returns></returns>
+        public static Maybe<T> Create(T value) {
+            //TODO: test for equality to null, in the case of a reference type
+            //if(Object.ReferenceEquals(data, null)){ } ??
+            return new Maybe<T>(value);
         }
 
+        /// <summary>
+        /// Returns an empty Maybe
+        /// </summary>
+        /// <returns></returns>
         public static Maybe<T> CreateEmpty() {
             return new Maybe<T>();            
         }
