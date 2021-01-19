8c51363d44b37d744a9102e0c48bef4307722996
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-18 19:50:38 -0500

    - simple Maybe implementation

--- task-scheduler-utility/Maybe.cs
+++ task-scheduler-utility/Maybe.cs
@@ -1,6 +1,28 @@
 ﻿using System;
 
 namespace task_scheduler_utility {
-    public class Maybe {
+    public class Maybe<T> {
+
+        private readonly T t;
+
+        public bool HasValue { get; private set; }
+        public T Value { get => t; }
+
+        private Maybe(T t) {
+            this.t = t;
+            HasValue = true;
+        }
+
+        private Maybe() {
+
+        }
+
+        public static Maybe<T> Create(T t) {
+            return new Maybe<T>(t);
+        }
+
+        public static Maybe<T> CreateEmpty() {
+            return new Maybe<T>();            
+        }
     }
 }
