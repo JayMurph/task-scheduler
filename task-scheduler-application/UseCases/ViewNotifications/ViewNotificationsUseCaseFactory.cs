33f7a5af278ece9ab4d3b25281f42d4e6e89f897
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-23 21:42:18 -0500

    NotificationRepositoryFactory in ViewNotificationsUseCaseFactory

--- task-scheduler-application/UseCases/ViewNotifications/ViewNotificationsUseCaseFactory.cs
+++ task-scheduler-application/UseCases/ViewNotifications/ViewNotificationsUseCaseFactory.cs
@@ -6,19 +6,19 @@ namespace task_scheduler_application.UseCases.ViewNotifications {
     public class ViewNotificationsUseCaseFactory : IUseCaseFactory<ViewNotificationsUseCase> {
 
         //dependencies required for ViewNotificaitonsUseCase
-        private readonly INotificationManager notificationManager;
+        private readonly INotificationRepositoryFactory notificationRepositoryFactory;
         private readonly ITaskItemRepositoryFactory taskItemRepositoryFactory;
 
         public ViewNotificationsUseCaseFactory(
-            INotificationManager notificationManager,
-            ITaskItemRepositoryFactory taskItemRepositoryFactory) {
+            ITaskItemRepositoryFactory taskItemRepositoryFactory, 
+            INotificationRepositoryFactory notificationRepositoryFactory) {
 
-            this.notificationManager = notificationManager ?? throw new ArgumentNullException(nameof(notificationManager));
             this.taskItemRepositoryFactory = taskItemRepositoryFactory ?? throw new ArgumentNullException(nameof(taskItemRepositoryFactory));
+            this.notificationRepositoryFactory = notificationRepositoryFactory ?? throw new ArgumentNullException(nameof(notificationRepositoryFactory));
         }
 
         public ViewNotificationsUseCase New() {
-            return new ViewNotificationsUseCase(notificationManager, taskItemRepositoryFactory);
+            return new ViewNotificationsUseCase(notificationRepositoryFactory, taskItemRepositoryFactory);
         }
     }
 }
