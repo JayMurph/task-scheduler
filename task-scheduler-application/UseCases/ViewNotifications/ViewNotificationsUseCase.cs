bff84da8b580bddd917a2d7b8f948fff9dfce825
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-23 21:45:29 -0500

    NotificationRepositoryFactory and NotificationRepository in ViewNotificationsUseCase

--- task-scheduler-application/UseCases/ViewNotifications/ViewNotificationsUseCase.cs
+++ task-scheduler-application/UseCases/ViewNotifications/ViewNotificationsUseCase.cs
@@ -9,18 +9,17 @@ using task_scheduler_utility;
 namespace task_scheduler_application.UseCases.ViewNotifications {
     public class ViewNotificationsUseCase : IUseCase<ViewNotificationsInput, ViewNotificationsOutput> {
 
-        //used for retrieving notifications present in the domain
-        private readonly INotificationManager notificationManager;
+        //used for retrieving notifications 
+        private readonly INotificationRepositoryFactory notificationRepositoryFactory;
 
         //used for retrieving data of task associated to a notification
         private readonly ITaskItemRepositoryFactory taskItemRepositoryFactory;
 
         public ViewNotificationsUseCase(
-            INotificationManager notificationManager,
-            ITaskItemRepositoryFactory taskItemRepositoryFactory) {
+            INotificationRepositoryFactory notificationRepositoryFactory, ITaskItemRepositoryFactory taskItemRepositoryFactory) {
 
-            this.notificationManager = notificationManager ?? throw new ArgumentNullException(nameof(notificationManager));
             this.taskItemRepositoryFactory = taskItemRepositoryFactory ?? throw new ArgumentNullException(nameof(taskItemRepositoryFactory));
+            this.notificationRepositoryFactory = notificationRepositoryFactory ?? throw new ArgumentNullException(nameof(notificationRepositoryFactory));
         }
 
         /// <summary>
@@ -39,16 +38,17 @@ namespace task_scheduler_application.UseCases.ViewNotifications {
              * Get all notifications that are present in the application, convert them to DTO's, then
              * add them to a collection to return
              */
-            foreach(Notification notification in notificationManager.GetAll()) {
+            INotificationRepository notificationRepository = notificationRepositoryFactory.New();
+            foreach(NotificationDAL notification in notificationRepository.GetAll()) {
 
-                Maybe<TaskItemDAL> maybeTask = taskRepo.GetById(notification.Producer.ID);
+                Maybe<TaskItemDAL> maybeTask = taskRepo.GetById(notification.taskId);
 
                 if(maybeTask.HasValue) {
                     TaskItemDAL taskDAL = maybeTask.Value;
                     NotificationDTO dto = new NotificationDTO() {
                         TaskId = taskDAL.id,
                         Title = taskDAL.title,
-                        Time = notification.Time,
+                        Time = notification.time,
                         R = taskDAL.r,
                         G = taskDAL.g, 
                         B = taskDAL.b
