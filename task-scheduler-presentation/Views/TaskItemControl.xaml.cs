ce0f821832e6c65ef15a38f753346be3b90148c9
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-29 20:59:49 -0500

    added DeleteClick event, OnDeleteClick, and DeleteMenuOption_Click

--- task-scheduler-presentation/Views/TaskItemControl.xaml.cs
+++ task-scheduler-presentation/Views/TaskItemControl.xaml.cs
@@ -1,10 +1,21 @@
-﻿using task_scheduler_presentation.Models;
+﻿using System;
+using System.ComponentModel;
+using task_scheduler_presentation.Models;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace task_scheduler_presentation.Views {
     public sealed partial class TaskItemControl : UserControl {
+
+        [Browsable(true)] [Category("Action")]
+        [Description("Invoked when delete context menu option is selected")]
+        public event EventHandler<TaskItemModel> DeleteClick;
+
+        private void OnDeleteClick(object sender, TaskItemModel model){
+            this.DeleteClick?.Invoke(sender, model);
+        }
 
         /// <summary>
         /// Serves as the Data that the Control markup accesses and displays
@@ -18,6 +29,10 @@ namespace task_scheduler_presentation.Views {
 
             //Update the Bindings whenever the DataContext is changed
             this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e) {
+            OnDeleteClick(this, TaskItem);
         }
     }
 }
