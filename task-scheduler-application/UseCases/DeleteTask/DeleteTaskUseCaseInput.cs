28cbf1b9843b02b3a14ce8778390c60a634e6473
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-31 21:06:10 -0500

    renaming

--- task-scheduler-application/UseCases/DeleteTask/DeleteTaskUseCaseInput.cs
+++ task-scheduler-application/UseCases/DeleteTask/DeleteTaskUseCaseInput.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace task_scheduler_application.UseCases.DeleteTask {
-    public class DeleteTaskUseCaseInput : UseCaseOutput{
+    /// <summary>
+    /// Encapsulates the input required for the Delete Task use case
+    /// </summary>
+    public class DeleteTaskUseCaseInput {
+        /// <summary>
+        /// The unique identifier of the TaskItem to be deleted
+        /// </summary>
+        Guid Id;
     }
 }
