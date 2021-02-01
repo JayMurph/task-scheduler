d3768adc7b323415ba11ed9e75d255c90f9a9176
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-31 21:04:29 -0500

    created DeleteTaskUseCaseOutput

---
+++ task-scheduler-application/UseCases/DeleteTask/DeleteTaskUseCaseOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_scheduler_application.UseCases.DeleteTask {
+    /// <summary>
+    /// Encapsulates the input required for the Delete Task use case
+    /// </summary>
+    public class DeleteTaskUseCaseOutput {
+        /// <summary>
+        /// The unique identifier of the TaskItem to be deleted
+        /// </summary>
+        Guid Id;
+    }
+}
