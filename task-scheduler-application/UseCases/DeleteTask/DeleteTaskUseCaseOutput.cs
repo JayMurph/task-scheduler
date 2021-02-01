85800fc6c5c945eb28beb943b72c5e42df600577
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-01-31 21:45:06 -0500

    comments

--- task-scheduler-application/UseCases/DeleteTask/DeleteTaskUseCaseOutput.cs
+++ task-scheduler-application/UseCases/DeleteTask/DeleteTaskUseCaseOutput.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace task_scheduler_application.UseCases.DeleteTask {
+    /// <summary>
+    /// Encapsulates the output of a DeleteTaskUseCase
+    /// </summary>
     public class DeleteTaskUseCaseOutput : UseCaseOutput{
     }
 }
