c7081dde7e3d7618c7053406dad3e1717a3292b1
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-02-03 20:37:16 -0500

    added id

--- task-scheduler-presentation/Models/TaskItemModel.cs
+++ task-scheduler-presentation/Models/TaskItemModel.cs
@@ -5,6 +5,7 @@ namespace task_scheduler_presentation.Models {
     /// Represents a TaskItem so it can be displayed in the presentation layer
     /// </summary>
     public class TaskItemModel {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime StartTime { get; set; }
