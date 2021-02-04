df2e45b187805fb9f4f04af669d6ddd0779c75a6
Joshua Murphy <50997667+JayMurph@users.noreply.github.com>
2021-02-03 20:41:00 -0500

    requiring model instead of just id requireing collection of TaskItems

--- task-scheduler-presentation/Views/IDeleteTaskView.cs
+++ task-scheduler-presentation/Views/IDeleteTaskView.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using task_scheduler_presentation.Models;
 
 namespace task_scheduler_presentation.Views {
     public interface IDeleteTaskView {
-        Guid TaskId { get; }
+        ObservableCollection<TaskItemModel> TaskItems { get; set; }
+
+        TaskItemModel ModelToDelete { get; set; }
     }
 }
